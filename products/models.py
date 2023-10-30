0d28f17ee0c8aa5b3ea745827487525d54e50df6
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-10-30 18:44:44 +0300

    add_basket

--- products/models.py
+++ products/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):  # Категории товаров
+    name = models.CharField(max_length=128, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):  # Продукты
+    CHOICES_SIZE = (
+        ('Size S', 'S'),
+        ('Size M', 'M'),
+        ('Size L', 'L'),
+        ('Size XL', 'XL'),
+    )
+
+    CHOICES_COLOR = (
+        ('red', 'Red'),
+        ('blue', 'Blue'),
+        ('white', 'White'),
+        ('grey', 'Grey'),
+
+
+    )
+
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    size = models.CharField(max_length=50, choices=CHOICES_SIZE)
+    color = models.CharField(max_length=50, choices=CHOICES_COLOR)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    image = models.ImageField(upload_to='products_image')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    class Meta:  # Отображение в админ панели
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
-
+from products.views import index, products, category_products_view, detail_view, about, detail_view_index
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
+    path('', index, name='index'),  # Отображение главной страницы
+    path('products/', products, name='products'),  # Отображение страницы с продуктами
+    # path('basket/', baskets, name='basket'),
+    path('product/<int:id>/', detail_view, name='details'),  # Отображение детализации продукции
+    path('product/<int:id>/', detail_view_index, name='details_index'),  # Отображение детализации продукции
+    path('about/', about, name='about'),  # Отображение информации о компании
+    path('products/category/<int:category_id>/', category_products_view, name='category_products'),  # Категории
 ]
