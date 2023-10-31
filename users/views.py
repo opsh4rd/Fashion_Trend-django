73e6ce25c5714cfdd7ab42fa3e2feb3acd6ce5aa
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-10-31 18:07:42 +0300

    add_login, registration

--- users/views.py
+++ users/views.py
@@ -1,15 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from users.forms import UserRegistrationForm, UserLoginForm
+from django.contrib import auth
 
 def login(request):
-    context = {
-        'title': 'Authorization',
-    }
-    return render(request, 'users/login.html', context)
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            # `authenticate` проверяет имя пользователя и пароль и возвращает `User` объект, если они правильные
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user:
+                auth.login(request, user)  # `login` устанавливает пользователя в текущую сессию
+                return redirect('index')  # перенаправление на главную страницу после входа
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
 
 
 def registration(request):
-    context = {
-        'title': 'Registration',
-    }
-    return render(request, 'users/registration.html', context)
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # перенаправление на страницу входа после успешной регистрации
+    else:
+        form = UserRegistrationForm()
+
+    return render(request, 'users/registration.html', {'form': form, 'title': 'Registration'})
