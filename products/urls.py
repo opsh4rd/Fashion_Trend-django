40efe2c8c76715d31ddbb4e436709d819b6f4b31
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-10-31 18:06:59 +0300

    add_url_contact

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from products.views import index, products, category_products_view, detail_view, about, detail_view_index
+from products.views import index, products, category_products_view, detail_view, about, detail_view_index, contacts
 
 urlpatterns = [
     path('', index, name='index'),  # Отображение главной страницы
@@ -9,5 +9,6 @@ urlpatterns = [
     path('product/<int:id>/', detail_view, name='details'),  # Отображение детализации продукции
     path('product/<int:id>/', detail_view_index, name='details_index'),  # Отображение детализации продукции
     path('about/', about, name='about'),  # Отображение информации о компании
+    path('contact/', contacts, name='contact'),  # Отображение информации о компании
     path('products/category/<int:category_id>/', category_products_view, name='category_products'),  # Категории
 ]
