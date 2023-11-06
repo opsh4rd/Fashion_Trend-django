bd108283a27102e24fdee76f68514b100edffa6e
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-11-06 17:18:49 +0300

    add_baskets

--- baskets/models.py
+++ baskets/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from users.models import User
+from products.models import Product
 
+
+class BasketQuerySet(models.QuerySet):
+    def total_sum(self):
+        return sum(basket.sum() for basket in self)
+
+    def total_quantity(self):
+        return sum(basket.quantity for basket in self)
+
+
+class Baskets(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=1)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = BasketQuerySet.as_manager()
+
+    def __str__(self):
+        return f'Корзина для {self.user.username} | Продукт: {self.product.name} | Количество: {self.quantity}'
+
+    def sum(self):
+        return self.product.price * self.quantity
 
