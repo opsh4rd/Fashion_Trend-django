c2889bebb61224c59e769d1aa367b6d3d1c538f7
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-11-08 11:00:59 +0300

    add_baskets

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from baskets.views import baskets
-
+from baskets.views import baskets, basket_add, basket_remove
 
 urlpatterns = [
     path('', baskets, name='basket'),
-
+    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
+    path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
 ]
--- baskets/views.py
+++ baskets/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+# from django.contrib.auth.decorators import login_required
+from products.models import Product
+from baskets.models import Baskets
+from products.models import ProductCategory
 
 
 def baskets(request):
+    if request.user.is_authenticated:
+        baskets = Baskets.objects.filter(user=request.user)
+    else:
+        baskets = []
+
     context = {
-        'title': 'Shoping Cart',
+        'title': 'Shopping Cart',
+        'baskets': baskets,
+        'categories': ProductCategory.objects.all(),
     }
+
     return render(request, 'baskets/baskets.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Baskets.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Baskets.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return render(request, 'baskets/baskets.html')
+    # return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Baskets.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
