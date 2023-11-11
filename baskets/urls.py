e503b8459efc08cd6d91c8a780fbb0a652586548
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-11-11 18:43:10 +0300

    add_+-

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from baskets.views import baskets, basket_add, basket_remove
+from baskets.views import baskets, basket_add, basket_remove, basket_negative
 
 urlpatterns = [
     path('', baskets, name='basket'),
-    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
-    path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+    path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),  # добавление в панели
+    path('baskets/negative/<int:product_id>/', basket_negative, name='basket_negative'),  # убавление в панели
+    path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),  # удаление в целом
 ]
--- baskets/views.py
+++ baskets/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import render, HttpResponseRedirect, reverse, get_object_or_404
 # from django.contrib.auth.decorators import login_required
 from products.models import Product
 from baskets.models import Baskets
 from products.models import ProductCategory
 
 
+# Отображение корзины товаров
 def baskets(request):
     if request.user.is_authenticated:
         baskets = Baskets.objects.filter(user=request.user)
@@ -20,6 +21,7 @@ def baskets(request):
     return render(request, 'baskets/baskets.html', context)
 
 
+# Добавление и прибавление товара
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Baskets.objects.filter(user=request.user, product=product)
@@ -31,11 +33,33 @@ def basket_add(request, product_id):
         basket.quantity += 1
         basket.save()
 
-    return render(request, 'baskets/baskets.html')
-    # return HttpResponseRedirect(request.META['HTTP_REFERER'])
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+# Убавление товара
+def basket_negative(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Baskets.objects.filter(user=request.user, product=product)
+
+    if baskets.exists():
+        basket = baskets.first()
+        if basket.quantity > 1:
+            basket.quantity -= 1
+            basket.save()
+        else:
+            basket.delete()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+# Удаление в целом товара
 def basket_remove(request, basket_id):
     basket = Baskets.objects.get(id=basket_id)
-    basket.delete()
+    baskets = Baskets.objects.filter(user=request.user)
+    del_basket = basket.delete()
+    context = {
+        'del_basket': del_basket,
+        'baskets': baskets,
+    }
+
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
